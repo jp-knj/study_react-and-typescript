3a6f4a372c2b40ad5e3ede966ba9440a60469248
jp-knj <kenji.tomita1996@gmail.com>
2021-04-12 19:54:28 +0900

    :+1: Add src
    t

---
+++ 01_react-and-typescript/src/index.tsx
@@ -0,0 +1,6 @@
+import Application from './Application';
+import { render } from 'react-dom';
+import './styles.scss';
+
+const rootElement = document.getElementById("root");
+render(<Application />, rootElement);
